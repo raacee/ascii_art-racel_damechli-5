593bf9d36b7b83f6134615b1f1adde81c7b3b941
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-01-29 18:46:56 +0100

    Add color choices

--- main.py
+++ main.py
@@ -9,10 +9,34 @@ def select_font():
     choice = input("Enter the number of your font choice : ")
     return fonts[int(choice) - 1] if choice.isdigit() and 0 < int(choice) <= len(fonts) else fonts[0]
 
+def color_text(text, color_code):
+    return f"\033[{color_code}m{text}\033[0m"
+
+def select_color():
+    colors = {
+        "red": "31",
+        "green": "32",
+        "yellow": "33",
+        "blue": "34",
+        "magenta": "35",
+        "cyan": "36",
+        "white": "37"
+    }
+    
+    print("Choose a color :")
+    for color in colors:
+        print(color)
+    
+    color_choice = input("Enter the name of the choosen color (let empty for no specific color) : ").lower()
+    return colors.get(color_choice, "")
+
 def main():
     s = input("Write something : ")
     font_choice = select_font()
-    tprint(s, font=font_choice)
+    color_code = select_color()
+    ascii_art = tprint(s, font=font_choice, chr_ignore=True)
+    colored_ascii_art = color_text(ascii_art, color_code) if color_code else ascii_art
+    print(colored_ascii_art)
 
 if __name__ == "__main__":
     main()
