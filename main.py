a87ceca35ff7c07da4eeba273a5bbc944fac90db
sebmoine <114504523+sebmoine@users.noreply.github.com>
2024-01-29 18:31:38 +0100

    Font choice

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from art import tprint
+from art import tprint, ART_FONTS
 
+def select_font():
+    print("Choisissez une police pour votre art ASCII :")
+    for i, font in enumerate(ART_FONTS, start=1):
+        print(f"{i}. {font}")
+    
+    choice = input("Entrez le numéro de votre choix de police : ")
+    return ART_FONTS[int(choice) - 1] if choice.isdigit() and 0 < int(choice) <= len(ART_FONTS) else ART_FONTS[0]
 
-s = input("Write something :")
-tprint(s)
+def main():
+    s = input("Write something : ")
+    font_choice = select_font()
+    tprint(s, font=font_choice)
+
+if __name__ == "__main__":
+    main()
